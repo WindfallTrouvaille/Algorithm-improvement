a2972ffe929c397a955f0873d0580e38fc133d60
Alice <1443573779@qq.com>
2021-04-09 21:50:09 +0800

    Mark

---
+++ Algorithm/Algorithmn/pro_字符串合并.java
@@ -0,0 +1,14 @@
+package Algorithmn;
+
+import java.util.Scanner;
+public class pro_字符串合并 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		//输入两个字符串
+		String n = input.next();
+		String m = input.next();
+		String result = n + m;
+		System.out.println(result);
+	}
+}
